b3d3f277a8c7396c809a2bb76d8621028d12a03d
Dung (Jun) Trinh <dungquoctrinh@users.noreply.github.com>
2022-03-24 22:03:41 -0700

    test check in

--- gulpfile.js
+++ gulpfile.js
@@ -76,4 +76,4 @@ gulp.task('watch', function() {
   gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
 });
 
-gulp.task('default', ['js', 'sass', 'browser-sync', 'watch']);
+gulp.task('default', ['js', 'sass', 'browser-sync', 'jekyll-build']);
